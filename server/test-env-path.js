deadd63fbd6a8a424762691d8e9dddf16c3f299a
aryan-2206 <aryandoshi22@gmail.com>
2026-06-08 16:13:04 +0530

    cleaning repo

--- server/test-env-path.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '.env') });
-
-console.log('Environment variables:');
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-console.log('PORT:', process.env.PORT);
-console.log('NODE_ENV:', process.env.NODE_ENV);
--- server/test-env.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-console.log('Environment variables:');
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-console.log('PORT:', process.env.PORT);
-console.log('NODE_ENV:', process.env.NODE_ENV);
